44e4a0201ea031d0e726494fed1f753ad4ff566f
salazarguille <guillesalazar@gmail.com>
2018-02-12 00:44:46 -0300

    #no-id
    Configuring port.

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const app = next({dev: process.env.NODE_ENV !== 'production'})
 const routes = require('./routes')
 const handler = routes.getRequestHandler(app)
 
+const PORT = process.env.PORT || 4537;
+
 // Without express
 app.prepare().then(() => {
-  createServer(handler).listen(3000, (err) => {
+  createServer(handler).listen(PORT, (err) => {
       if(err) throw err;
-      console.log('Ready on localhost:3000');
+      console.log('Ready on localhost:' + PORT);
   } );
 })
