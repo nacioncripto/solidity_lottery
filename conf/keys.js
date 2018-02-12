b712e336a68a0cc376346d67f48e928a4a768eaa
salazarguille <guillesalazar@gmail.com>
2018-02-12 00:09:49 -0300

    #no-id
    Adding log.

--- conf/keys.js
+++ conf/keys.js
@@ -1,8 +1,10 @@
 // keys.js - figure out what set of credentials to return.
 if (process.env.NODE_ENV == 'production') {
 	// we are in production - return the prod set of keys.
+	console.log('production');
 	module.exports = require('./rinkeby');
 } else {
 	// we are in development - return the dev keys.
+	console.log('dev');
 	module.exports = require('./dev');
 }
