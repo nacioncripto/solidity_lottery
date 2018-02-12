017fd6c8ed0fefa8bcb1d2a8500cf7c06cd09250
salazarguille <guillesalazar@gmail.com>
2018-02-12 01:00:32 -0300

    #no-id
    Log keys conf.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,8 @@ import web3 from './web3';
 import LotteryFactory from './build/LotteryFactory.json';
 import KEYS from '../conf/keys';
 
+console.log(KEYS);
+
 const instance = new web3.eth.Contract(
     JSON.parse(LotteryFactory.interface),
     KEYS.address
